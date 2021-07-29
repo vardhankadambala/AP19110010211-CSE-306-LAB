bc7c3d4521b4a908ae0187263daf580048ef644f
sai vardhan <85542869+vardhankadambala@users.noreply.github.com>
2021-07-30 01:49:13 +0530

    Update 2ndprogram.c

--- week-1/2ndprogram.c
+++ week-1/2ndprogram.c
@@ -2,72 +2,63 @@
 void main()
 {
     int state=0,i=0;
-    char token,input[20];
+    char token,input[20],y;
     printf("Enter input string \t :");
     scanf("%s",input);
     while((token=input[i++])!='\0')
     {
         switch(state)
         {
-            case 0: if(token=='a')
-                        state=1;
-                    else if(token=='b')
-                        state=3;
-                    else
-                    {
-                        printf("Invalid entry");
-                        
-                    }
-                    break;
+            case 0:
+                 if(token != y){
+                    state=1;
+                }
+                else if(token == y){
+                    state=2;
+                }
+                else
+                {
+                    printf("Invalid entry");
+                    exit(0);
+                }
+                break;
+                
+            case 1:
+                if(token==y){
+                    state=2;
+                }
+                else if(token != y){
+                    state=0;
+                }
+                else
+                {
+                    printf("Invalid entry");
+                    exit(0);
+                }
+                break;
                     
-            case 1: if(token=='a')
-                        state=2;
-                    else if(token=='b')
-                        state=3;
-                    else
-                    {
-                        printf("Invalid entry");
-                        
-                    }
-                    break;
-                    
-            case 2: if(token=='a')
-                        state=2;
-                    else if(token=='b')
-                        state=3;
-                    else
-                    {
-                        printf("Invalid entry");
-                        
-                    }
-                    break;
-                    
-            case 3: if(token=='a')
-                        state=1;
-                    else if(token=='b')
-                        state=4;
-                    else
-                    {
-                        printf("Invalid entry");
-                        
-                    }
-                    break;
-                    
-            case 4: if(token=='a')
-                        state=1;
-                    else if(token=='b')
-                        state=4;
-                    else
-                    {
-                        printf("Invalid entry");
-                        
-                    }
-                    break;
-                    
+            case 2: 
+                if(token == y){
+                    state=2;
+                }
+                else if(token != y){
+                    state=0;
+                }
+                else
+                {
+                    printf("Invalid entry");
+                    exit(0);
+                }
+                break;
         }
+        y = token;
     }
-    if(state==2 || state==4)
+
+
+    if(state==2){
         printf("\n\nString accepted\n\n");
-    else
+    }    
+    else{
         printf("\n\nString not accepted\n\n");
+    }
 }
